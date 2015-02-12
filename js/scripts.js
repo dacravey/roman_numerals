23ae040ad5805223a824264539fca9e3b6b8b8db
Debbie Cravey and Kathryn Carr <cinemaestro@yahoo.com>
2015-02-11 16:04:28 -0800

    add experiment for 99, 999, etc.

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,23 @@ var romanNums = function(arabicNumber){
   var values = [1000, 500, 100, 50, 10, 5, 1];
   var symbols = ["M", "D", "C", "L", "X", "V", "I"];
   var results = "";
+  var totalNumberString = totalNumber.toString();
 
 
   for(var i = 0; i < values.length; i += 1){
-      while(Math.floor(totalNumber/values[i]) > 0){
-        totalNumber = totalNumber - values[i];
-        results = results.concat(symbols[i]);
+     if(totalNumberString[0] === '9'){
+debugger;
+       results = results.concat(symbols[i+1]).concat(symbols[i-1]);
+       totalNumber = totalNumber - (9 * values[i+1]);
+       totalNumberString = totalNumber.toString();
+     }else{
+        while(Math.floor(totalNumber/values[i]) > 0){
+            totalNumber = totalNumber - values[i];
+            totalNumberString = totalNumber.toString();
+            results = results.concat(symbols[i]);
+          }
       }
   }
-
-
 
   return results;
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,29 +1,51 @@
 describe('romanNums', function(){
-  it("returns 'M' if user input 1,000", function(){
-    expect(romanNums(1000)).to.equal("M");
+  // it("returns 'M' if user input 1,000", function(){
+  //   expect(romanNums(1000)).to.equal("M");
+  // });
+  //
+  // it("returns 'D' if user input 500", function() {
+  //   expect(romanNums(500)).to.equal("D");
+  // });
+  //
+  // it("returns 'DC' if user input 600", function() {
+  //   expect(romanNums(600)).to.equal("DC");
+  // });
+  //
+  // it("returns 'MDCL' if user input is 1650", function() {
+  //   expect(romanNums(1650)).to.equal("MDCL");
+  // });
+  //
+  // it("returns 'MDCLX' if user input is 1660", function() {
+  //   expect(romanNums(1660)).to.equal("MDCLX");
+  // });
+  //
+  // it("returns 'MDCLXV' if user input is 1665", function() {
+  //   expect(romanNums(1665)).to.equal("MDCLXV");
+  // });
+  //
+  // it("returns 'MDCLXVI' if user input is 1666", function() {
+  //   expect(romanNums(1666)).to.equal("MDCLXVI");
+  // });
+  //
+  // // it("returns 'IX' if user input is 9", function() {
+  // //   expect(romanNums(9)).to.equal("IX");
+  // // });
+  // //
+  // // it("returns 'XXIX' if user input is 29", function() {
+  // //   expect(romanNums(29)).to.equal("XXIX");
+  // // });
+  // //
+  // // it("returns 'IV' if user input 4", function() {
+  // //   expect(romanNums(4)).to.equal("IV");
+  // // });
+  //
+  // it("returns 'MCM' if user input is 1900", function() {
+  //   expect(romanNums(1900)).to.equal("MCM");
+  // });
+
+  it("returns 'MCMXC' if user input is 1990", function() {
+    expect(romanNums(999)).to.equal("MCMXC");
   });
 
-  it("returns 'D' if user input 500", function() {
-    expect(romanNums(500)).to.equal("D");
-  });
 
-  it("returns 'DC' if user input 600", function() {
-    expect(romanNums(600)).to.equal("DC");
-  });
-
-  it("retuns 'MDCL' if user input is 1650", function() {
-    expect(romanNums(1650)).to.equal("MDCL");
-  });
-
-  it("returns 'MDCLX' if user input is 1660", function() {
-    expect(romanNums(1660)).to.equal("MDCLX");
-  });
-
-  it("returns 'MDCLXV' if user input is 1665", function() {
-    expect(romanNums(1665)).to.equal("MDCLXV");
-  });
-
-  it("returns 'MDCLXVI' if user input is 1666", function() {
-    expect(romanNums(1666)).to.equal("MDCLXVI");
-  });
 });
